5114825d8a9b42a34ddb8ec3b05b4cb01e3e4eb2
Alex Desjardins <ardesjardins91@gmail.com>
2020-12-09 17:43:36 -0700

    Solved Kids With the Greatest Number of Candies

--- leetcode.rb
+++ leetcode.rb
@@ -21,3 +21,25 @@ def running_sum(nums)
     nums.map{|num| sum+=num}
 end
 #------------------
+
+# Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+#
+# For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number
+# of candies among them. Notice that multiple kids can have the greatest number of candies.
+#
+# Input: candies = [2,3,5,1,3], extraCandies = 3
+# Expected Output: [true,true,true,false,true]
+
+def kids_with_candies(candies, extra_candies)
+    max = candies.max
+    candies.map do |c|
+        if c < max && c + extra_candies >= max
+            true
+        elsif c == max
+            true
+        else
+            false
+        end
+    end
+end
+#-----------------
